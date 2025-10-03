cfde953a9eac02d931995bfbc1f7beeaac831f97
preciousdev001 <preciouscheng.dev@gmail.com>
2025-10-03 16:26:21 -0700

    starter code and solutions
    starter codes and solutions

---
+++ Solution-3/space-mission-control/src/components/MissionFilter.jsx
@@ -0,0 +1,24 @@
+import styles from "./MissionFilter.module.css";
+
+function MissionFilter ({setFilter})
+{
+	const STATUSES = ["All", "Planned", "Active", "Completed"];
+
+	return (
+		<>
+			{
+				STATUSES.map((status, index) => (
+					<button
+						key={index}
+						onClick={() => setFilter(status)}
+						className={styles.button}
+					>
+						{status}
+					</button>
+				))
+			}
+		</>
+	);
+}
+
+export default MissionFilter;
